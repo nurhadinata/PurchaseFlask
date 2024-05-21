46bc31899c7f45d80afaf38734dae9c7a105fd9e
Cahya Nurhadinata <59736388+nurhadinata@users.noreply.github.com>
2024-05-21 00:08:21 -0700

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,3 +1,9 @@
+@echo off
+
+REM echo Installing Git Bash...
+REM winget install --id Git.Git -e
+
+REM winget install --id Google.Chrome -e
 set "GIT_BASH_PATH=C:\Program Files\Git\bin\bash.exe"
-set "SCRIPT_PATH=C:\path\to\your\script.sh"  REM Modify this line with your actual script path
+set "SCRIPT_PATH=C:\Scripts\test.sh"
 start "" "%GIT_BASH_PATH%" "%SCRIPT_PATH%"
