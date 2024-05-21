7183c9f40ffb8e98819c5daaa50f48fa0298c08b
Cahya Nurhadinata <59736388+nurhadinata@users.noreply.github.com>
2024-05-21 17:05:45 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -113,7 +113,7 @@ install_pyenv(){
 		
 		echo 'export PYENV_ROOT="$HOME/.pyenv"'>>~/.bashrc
 		echo 'export PATH="$PYENV_ROOT/bin:$PATH"'>>~/.bashrc
-		echo 'eval "$(pyenv init --path)"'>>~/.bashrc
+		echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
 
 		source ~/.bashrc
     fi
